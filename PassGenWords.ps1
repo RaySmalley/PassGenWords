29bab39afb5b9fff5b24b13c5a9bcd4275b9f0d6
Raymon Smalley <raysmalley@hotmail.com>
2022-05-25 13:34:45 -0400

    Update PassGenWords.ps1

--- PassGenWords.ps1
+++ PassGenWords.ps1
@@ -23,12 +23,16 @@ function Download {
     New-Variable -Name $Name"Output" -Value $Output -Scope Global -Force
 }
 
-Download -Name WordList -URL https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-no-swears.txt -Extension txt
-
-$FirstWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
-$SecondWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
-$ThirdWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
-
-Write-Host $FirstWord -ForegroundColor Cyan -NoNewline
-Write-Host $SecondWord -ForegroundColor Green -NoNewline
-Write-Host $ThirdWord -ForegroundColor Yellow
+function pgw {
+    Download -Name WordList -URL https://github.com/RaySmalley/PassGenWords/raw/main/WordList.txt -Extension txt
+    $FirstWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
+    $SecondWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
+    $ThirdWord = (Get-Culture).TextInfo.ToTitleCase($(Get-Random (Get-Content $WordListOutput | where {$_.Length -gt 4})))
+    Write-Host "Password added to clipboard: " -ForegroundColor Cyan -NoNewline
+    Write-Host $FirstWord -ForegroundColor Red -NoNewline
+    Write-Host - -NoNewline
+    Write-Host $SecondWord -ForegroundColor Yellow -NoNewline
+    Write-Host - -NoNewline
+    Write-Host $ThirdWord -ForegroundColor Green
+    Set-Clipboard $FirstWord-$SecondWord-$ThirdWord
+}
